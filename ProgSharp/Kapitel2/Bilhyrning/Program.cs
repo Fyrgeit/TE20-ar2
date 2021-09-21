7099a85b28898b5f63704e64ffcbd72c3adbfa29
Mans_Henriksson <henriksson.mans@elev.ga.ntig.se>
2021-09-21 11:06:27 +0200

    Gjorde uppgiften om biluthyrning

---
+++ ProgSharp/Kapitel2/Bilhyrning/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bilhyrning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jaså det är dags att hyra en bil!");
+            Console.WriteLine("Hur många dagar vill min unge herre hyra bilen?");
+
+            int dagar = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Absolut, absolut.");
+            Console.WriteLine("Och hur många kilometer har ni tänkt köra?");
+
+            int kilometer = int.Parse(Console.ReadLine());
+            
+            int hyra = 300 + (dagar - 1) * 500 + kilometer;
+
+            Console.WriteLine($"Ja, då stiger hyran upp till hela {hyra} riksdaler!");
+        }
+    }
+}
