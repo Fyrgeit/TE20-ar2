77b0455e37552450f7247964bf4db47805eb696a
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2021-09-29 11:05:31 +0200

    bool

---
+++ ProgSharp/Kapitel3/boolskaOperationer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace boolskaOperationer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
