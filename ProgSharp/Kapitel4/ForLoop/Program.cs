5a3995ebbfd12ff7dd20f8ae5b269167b1848ca7
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2021-10-19 12:23:06 +0200

    2048

---
+++ ProgSharp/Kapitel4/ForLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("╔════╗ ╔════╗ ╔════╗ ╔════╗");
+            Console.WriteLine("║1024║ ║1024║ ║1024║ ║1024║");
+            Console.WriteLine("╚════╝ ╚════╝ ╚════╝ ╚════╝");
+            Console.WriteLine("x────x x────x x────x x────x");
+            Console.WriteLine("│0000│ │0000│ │0000│ │0000│");
+            Console.WriteLine("x────x x────x x────x x────x");
+            Console.WriteLine("╬════╬ ╬════╬ ╬════╬ ╬════╬");
+            Console.WriteLine("║2048║ ║2048║ ║2048║ ║2048║");
+            Console.WriteLine("╬════╬ ╬════╬ ╬════╬ ╬════╬");
+            Console.WriteLine(" ----   ┄┄┄┄   ┄┄┄┄   ┄┄┄┄ ");
+            Console.WriteLine("|    | ┆    ┆ ┆    ┆ ┆    ┆");
+            Console.WriteLine(" ----   ┄┄┄┄   ┄┄┄┄   ┄┄┄┄ ");
+        }
+    }
+}
