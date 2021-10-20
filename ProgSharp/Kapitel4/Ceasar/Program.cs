4bd64129e968a0cb894304a3918a482bcc8fefc9
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2021-10-20 12:03:46 +0200

    hej

---
+++ ProgSharp/Kapitel4/Ceasar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ceasar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Input a string that you want to cipher.");
+            string input = Console.ReadLine();
+
+            int letter = 40;
+            int shortPunct = 200;    
+            int longPunct = 500;    
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                Console.Write(input[i]);
+                
+                if (input[i] == '.' || input[i] == '!' || input[i] == '?')
+                {
+                    System.Threading.Thread.Sleep(longPunct);
+                }
+                else if (input[i] == ',')
+                {
+                    System.Threading.Thread.Sleep(shortPunct);
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(letter);
+                }
+            }
+        }
+    }
+}
