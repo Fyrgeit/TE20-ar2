3a554e1444075e2250576e2a3215e6d724f2d7e2
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2021-10-26 10:44:51 +0200

    Cesar labb

---
+++ Labbar/Labb1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Labb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            
+            Console.WriteLine("Skriv in ett meddelande du vill kryptera.");
+            string input = Console.ReadLine();
+
+            Console.WriteLine($"Med hur många steg vill du förskjuta meddelandet?");
+            int shift = int.Parse(Console.ReadLine());
+
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+            Console.WriteLine($"Vill du använda det vanliga engelska alfabetet? (j/n)");
+            
+            if (Console.ReadLine() == "n")
+            {
+                Console.WriteLine($"Skriv in det alfabetet du vill använda. (Alla bokstäver, inga mellanslag)");
+                alphabet = Console.ReadLine().ToLower();
+            }
+
+            int intput = 0;
+            string output = "";
+            
+            input = input.ToLower();
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (alphabet.Contains(input[i]))
+                {
+                    for (var j = 0; j < alphabet.Length; j++)
+                    {
+                        if (input[i] == alphabet[j])
+                        {
+                            intput = j;
+                        }
+                    }
+
+                    intput += shift;
+
+                    if (intput >= alphabet.Length)
+                    {
+                        intput -= alphabet.Length;
+                    }
+
+                    output += alphabet[intput];
+                }
+                else
+                {
+                    output += input[i];
+                }
+            }
+
+            Console.WriteLine($"Ditt krypterade meddelande är:");
+            Console.WriteLine($"{output}");
+        }
+    }
+}
