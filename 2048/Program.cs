6e8e2def8b59c4e835697ddbc83ec7314163cc47
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2021-10-27 11:04:15 +0200

    2048

---
+++ 2048/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _2048
+{
+    class Program
+    {
+        public static int getnum(int n, string map)
+        {
+            return (int)map[n]-97;
+        }
+
+        static void Main(string[] args)
+        {
+            string map = "aaabaacbacddbbde";
+
+            for (var y = 0; y < 4; y++)
+            {
+                for (var x = 0; x < 4; x++)
+                {
+                    Console.Write(getnum(x+y*4, map));
+                }   
+                Console.Write("\n");
+            }
+
+            // /------\
+            // |      |
+            // |-2048-|
+            // |      |
+            // \------/
+        }
+    }
+}
