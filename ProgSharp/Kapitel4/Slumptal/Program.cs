bf300b142c121c50b906366ca083a9fef1be290d
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2021-10-27 10:22:16 +0200

    tärningskast

---
+++ ProgSharp/Kapitel4/Slumptal/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Slumptal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int tal1 = 0, tal2 = 0;
+            int result;
+            int täljare = 0;
+            int nämnare = 100000;
+            
+            for (int i = 0; i < nämnare; i++)
+            {
+                tal1 = random.Next(0, 7);
+                tal2 = random.Next(0, 7);
+                result = tal1 + tal2;
+                Console.WriteLine($"{i}: [{tal1} + {tal2} = {result}]");
+                if (result == 12)
+                {
+                    täljare++;
+                }
+                //System.Threading.Thread.Sleep(10);
+            }
+
+            Console.WriteLine($"Hittade {täljare} tolvor på {nämnare} tärningskast.");
+            
+        }
+    }
+}
