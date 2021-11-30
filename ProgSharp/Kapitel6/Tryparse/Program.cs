5c622141e7c90868cb5af66ce391a970d3ceb7e3
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2021-11-30 10:38:02 +0100

    sadje

---
+++ ProgSharp/Kapitel6/Tryparse/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Tryparse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iTal = 0;
+            bool intQ = false;
+
+            while (true)
+            { 
+                Console.Clear();
+
+                intQ = false;
+                while (!intQ)
+                {
+                    Console.WriteLine("Välj ett tal mellan 1 och 10");
+                    string sTal = Console.ReadLine();
+                    intQ = int.TryParse(sTal, out iTal);
+                }
+                
+                Random rnd = new Random();
+
+                int slumptal = rnd.Next(10)+1;
+
+                if (iTal == slumptal)
+                {
+                    Console.WriteLine($"GG!");
+                    Console.WriteLine($"Talet var {slumptal}!");
+                }
+                else
+                {
+                    Console.WriteLine($"Fel.");
+                    Console.WriteLine($"Talet var {slumptal}.");
+                }
+
+                Console.WriteLine($"Vill du köra igen? (y/n)");
+                if (Console.ReadKey().KeyChar != 'y')
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
