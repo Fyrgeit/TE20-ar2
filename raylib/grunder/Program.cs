af4065cb8ff7228df0d3cc16e9846fd56492f18f
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2022-01-12 10:27:14 +0100

    start

---
+++ raylib/grunder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Raylib_cs;
+
+namespace grunder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 500, "raylibtest");
+
+            Raylib.SetTargetFPS(60);
+
+            int x = 50;
+            int y = 50;
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.BLUE);
+
+                if (Raylib.IsKeyPressed(KEY_RIGHT))
+                {
+                    x++;
+                }
+
+                Raylib.DrawRectangle(x, y, 50, 50, Color.WHITE);
+
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
