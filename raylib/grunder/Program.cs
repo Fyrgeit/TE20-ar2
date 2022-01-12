88882bfa3c9a92ccfa4e75c9fed58bf00aaf7dc7
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2022-01-12 11:03:07 +0100

    spel

--- raylib/grunder/Program.cs
+++ raylib/grunder/Program.cs
@@ -11,21 +11,35 @@ namespace grunder
 
             Raylib.SetTargetFPS(60);
 
+            Color blue = new Color(0, 9, 171, 255);
+            Color red = new Color(222, 33, 16, 255);
+
             int x = 50;
             int y = 50;
+
+            int speed = 5;
 
             while (!Raylib.WindowShouldClose())
             {
                 Raylib.BeginDrawing();
 
-                Raylib.ClearBackground(Color.BLUE);
+                Raylib.ClearBackground(Color.WHITE);
+                
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+                {x+=speed;}
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+                {x-=speed;}
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+                {y+=speed;}
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+                {y-=speed;}
 
-                if (Raylib.IsKeyPressed(KEY_RIGHT))
-                {
-                    x++;
-                }
-
-                Raylib.DrawRectangle(x, y, 50, 50, Color.WHITE);
+                Raylib.DrawCircle(400, 250, 200, red);
+                Raylib.DrawCircle(400, 250, 130, Color.WHITE);
+                Raylib.DrawRectangle(150, 210, 500, 80, blue);
+                Raylib.DrawText("UNDERGROUND", 175, 223, 60, Color.WHITE);
+                
+                Raylib.DrawCircle(x, y, 30, Color.BLUE);
 
                 Raylib.EndDrawing();
             }
