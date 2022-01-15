c0c2d50c46e6da4e68e2023da3a1273983de8beb
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2022-01-15 14:18:28 +0100

    yujk

---
+++ raylib/Raket/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Raket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int screenWidth = 800;
+            const int screenHeight = 500;
+
+            float x = 200;
+            float y = 200;
+
+            Raylib.InitWindow(screenWidth, screenHeight, "raket");
+            
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.WHITE);
+                Raylib.DrawRectangle((int)x, (int)y, 20, 50, Color.RED);
+
+
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
