10c7aabfd95bb0e2a35d8192b75b9f97f4d36538
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2022-01-17 13:24:33 +0100

    kinda

--- raylib/Raket/Program.cs
+++ raylib/Raket/Program.cs
@@ -10,18 +10,44 @@ namespace Raket
             const int screenWidth = 800;
             const int screenHeight = 500;
 
-            float x = 200;
-            float y = 200;
+            //float x = 200;
+            //float y = 200;
+
+            float xv = 0;
+            float yv = 0;
 
             Raylib.InitWindow(screenWidth, screenHeight, "raket");
+            Raylib.SetTargetFPS(60);
+
+            Rectangle raket = new Rectangle();
+            raket.x = 200;
+            raket.y = 200;
+            raket.width = 20;
+            raket.height = 50;
+
+            Vector2 vektår = new Vector2();
+            vektår.x = 1;
+            vektår.y = 1;
+
             
             while (!Raylib.WindowShouldClose())
             {
                 Raylib.BeginDrawing();
-
                 Raylib.ClearBackground(Color.WHITE);
-                Raylib.DrawRectangle((int)x, (int)y, 20, 50, Color.RED);
 
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_SPACE))
+                {
+                    yv -= 0.25f;
+                }
+
+                //gravity
+                yv += 0.1f;
+
+                //simulate
+                raket.x += xv;
+                raket.y += yv;
+
+                Raylib.DrawRectangleRec(raket, vektår, 0, Color.RED);
 
                 Raylib.EndDrawing();
             }
